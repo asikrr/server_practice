5a63a345fffdd2c673131b3c2a15cb45fa4483f6
gr432_tkaev <asav32048@gmail.com>
2026-04-15 11:46:06 +0700

    use illuminate/database and illuminate/events; use models

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,18 +2,19 @@
 
 namespace Controller;
 
+use Model\Post;
 use Src\View;
 
 class Site
 {
-   public function index(): string
-   {
-       $view = new View();
-       return $view->render('site.hello', ['message' => 'index working']);
-   }
+    public function index(): string
+    {
+        $posts = Post::all();
+        return (new View())->render('site.post', ['posts' => $posts]);
+    }
 
-   public function hello(): string
-   {
-       return new View('site.hello', ['message' => 'hello working']);
-   }
+    public function hello(): string
+    {
+        return new View('site.hello', ['message' => 'hello working']);
+    }
 }
