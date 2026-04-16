8c582f0ad95362fcf1b1b32c3a742341bbb91cdc
gr432_tkaev <asav32048@gmail.com>
2026-04-16 23:29:08 +0700

    add trim middleware

--- config/app.php
+++ config/app.php
@@ -10,5 +10,8 @@ return [
     'validators' => [
         'required' => \Validators\RequireValidator::class,
         'unique' => \Validators\UniqueValidator::class
-    ]
+    ],
+    'routeAppMiddleware' => [
+        'trim' => \Middlewares\TrimMiddleware::class,
+    ],
 ];
